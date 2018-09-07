89acdb2f749da710207614944963abe05a5fe3dc
Никита Кураев <ellusionist.nick@mail.ru>
2018-09-07 21:42:50 +0300

    the lst one

---
+++ LoD/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace LoD
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label2 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(75, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Ингридиенты";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(94, 9);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(679, 20);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(337, 153);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Старт";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(194, 43);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown1.TabIndex = 3;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 45);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(176, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Количество страниц для анализа";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(15, 69);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(251, 17);
+			this.radioButton1.TabIndex = 5;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Искать только по введенным ингридиентам";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(15, 92);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(399, 17);
+			this.radioButton2.TabIndex = 6;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "При поиске учитывать возможность отклонений от списка ингридиентов";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 188);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Поиск рецептов";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+	}
+}
+
---
+++ LoD/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace LoD
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		public class Example
+		{
+			[JsonProperty("title")]
+			public string title { get; set; }
+
+			[JsonProperty("href")]
+			public string href { get; set; }
+
+			[JsonProperty("ingredients")]
+			public string ingredients { get; set; }
+
+			[JsonProperty("thumbnail")]
+			public string thumbnail { get; set; }
+		}
+
+		public class Example1
+		{
+			[JsonProperty("title")]
+			public string title { get; set; }
+
+			[JsonProperty("version")]
+			public string version { get; set; }
+
+			[JsonProperty("href")]
+			public string href { get; set; }
+
+			[JsonProperty("results")]
+			public List<Example> results { get; set; }
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			string result = "";
+			string result1 = "";
+			int ingcount = 0;
+			int i = 0;
+			do
+			{
+				i++;
+
+				string url = "http://www.recipepuppy.com/api/?i=" + textBox1.Text + "&p=" + i;
+
+				var json = new WebClient().DownloadString(url);
+				Example1 j1 = JsonConvert.DeserializeObject<Example1>(json);
+
+				if (j1.results.Count() == 0) break;
+
+				foreach (Example ex in j1.results)
+				{
+					if (result == "" && checkRadio(ex.ingredients.Split(new Char[] { ' ', ',' })))
+					{
+						result = ex.href;
+						result1 = ex.title;
+						ingcount = ex.ingredients.Split(new Char[] { ' ', ',' }).Count();
+					}
+					else if (ex.ingredients.Split(new Char[] { ' ', ',' }).Count() < ingcount && checkRadio(ex.ingredients.Split(new Char[] { ' ', ',' })))
+					{
+						result = ex.href;
+						result1 = ex.title;
+						ingcount = ex.ingredients.Split(new Char[] { ' ', ',' }).Count();
+					}
+				}
+
+			} while (i <= numericUpDown1.Value);
+
+			if (result != "")
+			{
+				MessageBox.Show("Название блюда - " + result1 + "; ссылка на него - " + result);
+			}
+			else
+			{
+				MessageBox.Show("К сожалению, по введенным вами пораметрам не было найдено ни одного рецепта");
+			}
+		}
+
+		public bool checkRadio(string[] s)
+		{
+			if (radioButton2.Checked || !radioButton1.Checked) return true;
+
+			string[] a = textBox1.Text.Split(new Char[] { ' ', ',' });
+
+			for (int i = 0; i < s.Count(); i++)
+			{
+				for (int j = 0; j < a.Count(); j++)
+				{
+					if (s[i] == a[j]) return false;
+				}
+			}
+
+			return true;
+
+		}
+	}
+}
